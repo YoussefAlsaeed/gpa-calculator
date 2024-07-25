3e1ab064f8444e40142d3c73a0795d0d47fa11ef
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2024-07-26 01:16:35 +0300

    linux support

---
+++ run-calc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install the required Python packages
+pip install -r requirements.txt
+
+# Run the fetch.py script
+python fetch.py
+
+# Run the calculate.py script
+python calculate.py
